2f2f030eea6c2aea40591ce4e2f7f31b3b299704
phpc99 <up202102365@up.pt>
2024-10-07 23:43:02 +0100

    Application class updated

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -8,26 +8,8 @@ import com.googlecode.lanterna.terminal.Terminal;
 import java.io.IOException;
 
 public class Application {
-    public static void main(String[] args) throws IOException {
-
-        Screen screen = null;
-        try {
-            Terminal terminal = new DefaultTerminalFactory().createTerminal();
-            screen = new TerminalScreen(terminal);
-            screen.setCursorPosition(null);     // we don't need a cursor
-            screen.startScreen();               // screens must be started
-            screen.doResizeIfNecessary();       // resize screen if necessary
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        TerminalSize terminalSize = new TerminalSize(40, 20);
-        DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
-        //Terminal terminal = terminalFactory.createTerminal();
-
-        screen.clear();
-        screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')
-                [0]);
-        screen.refresh();
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.run();
     }
 }
